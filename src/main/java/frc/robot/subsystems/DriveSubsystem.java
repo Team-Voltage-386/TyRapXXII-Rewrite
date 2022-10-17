84a4ddda50cbbe0b160e33d47fc501d8199402b9
Carl <55468526+Carlthird@users.noreply.github.com>
2022-10-17 19:30:05 -0400

    Update DriveSubsystem.java

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -1,9 +1,12 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DriveSubsystem extends SubsystemBase {
-    public final CANSparkMax frontLeftMotor = new CANSparkMax(Constants.DriveConstants.kFrontLeft, MotorType.kBrushless);
+    public final CANSparkMax frontLeftMotor = new CANSparkMax(2, MotorType.kBrushless);
 
     @Override
     public void periodic() {
