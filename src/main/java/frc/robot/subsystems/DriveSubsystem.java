91c40519adc805c642e0e56c13725ccc6a0b18dc
Carl <55468526+Carlthird@users.noreply.github.com>
2022-10-17 19:21:19 -0400

    Create DriveSubsystem.java

---
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -0,0 +1,12 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DriveSubsystem extends SubsystemBase {
+
+    @Override
+    public void periodic() {
+        
+    }
+    
+}
